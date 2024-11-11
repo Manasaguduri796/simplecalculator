97e69f9ea19aefba0bb63d91db17253b0831af22
manasa <manuguduri79@gmail.com>
2024-11-11 19:18:50 +0530

    message

--- simplecalculator.py
+++ simplecalculator.py
@@ -33,7 +33,7 @@ def calculate():
 label_result = Label(root,width=25,height=2,text="",font=("arial",30))
 label_result.pack()
 
-Button(root,text="c",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#3697f5.",command=lambda: clear()).place(x=10,y=100)
+Button(root,text="c",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#3697f5",command=lambda: clear()).place(x=10,y=100)
 Button(root,text="/",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("/")).place(x=150,y=100)
 Button(root,text="%",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("%")).place(x=290,y=100)
 Button(root,text="*",width=5,height=1,font=("arial",30,"bold"),bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("*")).place(x=430,y=100)
